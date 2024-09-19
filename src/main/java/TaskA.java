d8b7eece102868c42ca2b77d1899ab4634bee87d
aalapsley <114630770+aalapsley1@users.noreply.github.com>
2024-09-19 16:18:03 -0400

    task A

---
+++ src/main/java/TaskA.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class TaskA {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            //StringTokenizer itr = new StringTokenizer(value.toString());
+            String dataset = value.toString();
+            String[] datapoint = dataset.split("/n");
+            String[][] columns = new String[datapoint.length][];
+            for(int i = 0; i < columns.length; i++){
+                columns[i] = datapoint[i].split(",");
+            }
+
+
+            for (String[] column : columns) {
+                word.set(column[4]);
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public void debug(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(TaskA.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(TaskA.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path("input/LinkBookPage.csv"));
+        FileOutputFormat.setOutputPath(job, new Path("TaskAOutput"));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
--- src/main/java/TaskB.java
+++ src/main/java/TaskB.java
@@ -1,6 +1,3 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -11,7 +8,9 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCount {
+import java.io.IOException;
+
+public class TaskB {
 
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable>{
@@ -31,7 +30,7 @@ public class WordCount {
 
 
             for (String[] column : columns) {
-                word.set(column[0]);
+                word.set(column[2]);
                 context.write(word, one);
             }
         }
@@ -53,13 +52,46 @@ public class WordCount {
         }
     }
 
+
+    public static class SortMapper
+            extends Mapper<Text, IntWritable, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+
+        public void map(Text key, IntWritable value, Mapper.Context context
+        ) throws IOException, InterruptedException {
+
+            String dataset = value.toString();
+            String[] datapoint = dataset.split("/n");
+            String[][] columns = new String[datapoint.length][];
+            for (int i = 0; i < columns.length; i++) {
+                columns[i] = datapoint[i].split(",");
+            }
+
+            for (String[] column : columns) {
+                word.set(column[1]);
+                context.write(word, column[0]);
+            }
+
+        }
+    }
+
+
+
+
+
     public void debug(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
+        job.setJarByClass(TaskB.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
+
+        job.setMapperClass(SortMapper.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
@@ -70,14 +102,18 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
+        job.setJarByClass(TaskB.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
+
+        job.setMapperClass(SortMapper.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path("/Users/nicoleburgess/CS/Grad Year/CS585/Project-0/data.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("/Users/nicoleburgess/CS/Grad Year/CS585/Project-0/output1"));
+        FileInputFormat.addInputPath(job, new Path("TestaccessLogs.csv"));
+        //FileInputFormat.addInputPath(job, new Path("input/accessLogs.csv"));
+        FileOutputFormat.setOutputPath(job, new Path("TaskBOutput"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
